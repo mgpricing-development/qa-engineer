4000f9be5812284ece9dd41b3f809217d373434a
Edgar Ngwenya <edgar@cuvama.com>
2022-06-27 09:32:21 +0100

    Initial commit

---
+++ cypress/integration/tasks.spec.js
@@ -0,0 +1,11 @@
+describe("Task Page", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("Page Exists", () => {});
+
+  it("Create a New Task", () => {});
+
+  it("Delete a Task", () => {});
+});
